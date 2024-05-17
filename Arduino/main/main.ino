d55d717a4d5dabb972dd4193db4d40c07c25257e
Theluke2k <lselmer@gmail.com>
2024-05-17 14:33:32 +0200

    Update main.ino

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -898,7 +898,7 @@ void ControlTask(void* pvParameters) {
         }
       }*/
 
-      snprintf(buffer, sizeof(buffer), "%lu,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f",
+      snprintf(buffer, sizeof(buffer), "%lu,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f",
                millis(),
                q_d(0),
                q_d(1),
@@ -923,7 +923,20 @@ void ControlTask(void* pvParameters) {
                M3.state.qd,
                M4.state.qd,
                M5.state.qd,
-               M6.state.qd);
+               M6.state.qd,
+               tau(0),
+               tau(1),
+               tau(2),
+               tau(3),
+               tau(4),
+               tau(5),
+               M1.input,
+               M2.input,
+               M3.input,
+               M4.input,
+               M5.input,
+               M6.input
+               );
 
       Serial.println(buffer);
 
